a2a28f206cd01123decc1f8b7e0de8ca625074c0
SC Khoo <sckhoo@gmail.com>
2023-11-30 21:38:37 +0800

    at home

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ app = FastAPI()
 
 @app.get("/", tags=["root"])
 async def root():
-    return {"message": "If you see this, the Github trigger cloud build work"}
+    return {"message": "If you see this, the Github trigger cloud build work. at even at home Mac Mini. also work!!!"}
