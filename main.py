206a2440f35b5bfaf0ee7c82968c7877ba1c4672
SC Khoo <sckhoo@gmail.com>
2023-12-01 11:36:11 +0800

    added other verbs

--- main.py
+++ main.py
@@ -1,7 +1,38 @@
 from fastapi import FastAPI
+from typing import Optional
 
 app = FastAPI()
 
+items = {
+    1: {"name": "Honda", "price": 10.99},
+    2: {"name": "Yamaha", "price": 9.99},
+    3: {"name": "Kawasaki", "price": 12.99},
+}
+
 @app.get("/", tags=["root"])
 async def root():
-    return {"message": "If you see this, the Github trigger cloud build work. at even at home Mac Mini. also work!!!"}
+    return {"message": "Welcome to My API Server on Cloud Run"}
+
+@app.get("/items/{item_id}")
+async def get_data(item_id: int):
+    return {"message": "Data retrieve successfully!"}
+
+@app.put("/items/{item_id}")
+async def update_data(item_id: int, data: dict):
+    print(item_id)
+    return {"message": "Data updated successfully!"}
+
+@app.post("/add")
+async def create_data(data: dict):
+    print(data)
+    return {"message": "Data created successfully!"}
+
+@app.delete("/items/{item_id}")
+async def delete_data(item_id: int, data: dict):
+    print(data)
+    return {"message": "Data deleted successfully!"}
+
+@app.patch("/items/{item_id}")
+async def patch_data(item_id: int, data: dict):
+    print(item_id, data)
+    return {"message": "Data patched successfully!"}
