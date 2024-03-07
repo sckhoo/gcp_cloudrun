bfb2d7dc7ce8981f9f68d0efb3131bcc6116ee72
sckhoo <sckhoo@gmail.com>
2024-03-07 11:07:17 +0800

    v5

--- main.py
+++ main.py
@@ -24,6 +24,6 @@ def shutdown_db_client():
 
 @app.get("/", tags=["root"])
 async def root():
-    return {"message": "Welcome to My API Server on Cloud Run v4"}
+    return {"message": "Welcome to My API Server on Cloud Run v5"}
 
 app.include_router(items_router, tags=["items"], prefix="/items")
